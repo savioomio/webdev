91932424826c9cb6120feeb3fbb77b45df17f889
savioomio <saviopessoaafonso@gmail.com>
2026-04-08 20:04:29 -0300

    feat: exercicio 2

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+/*
 const nome = prompt("nome: ")
 const idade = prompt("idade: ")
 const curso = prompt("curso: ")
 
 alert(`Olá, meu nome é ${nome} tenho ${idade} anos e estudo ${curso} na FIAP.`)
+*/
+
+let distancia = prompt("Qual a distancia em Km: ")
+
+let centimetros = distancia * 100
+
+alert(`Distancia em centimetros: ${centimetros}cm`)
