94e32e38ed60df13005351738a4365977364052a
savioomio <saviopessoaafonso@gmail.com>
2026-04-08 20:22:01 -0300

    feat: exercicio 4

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-/*
-
 // Exercicio 1
 
+/*
 const nome = prompt("nome: ")
 const idade = prompt("idade: ")
 const curso = prompt("curso: ")
 
 alert(`Olá, meu nome é ${nome} tenho ${idade} anos e estudo ${curso} na FIAP.`)
-
 */
 
 // Exercicio 2
 
 /*
-
 let distancia = Number(prompt("Qual a distancia em Metros: "))
 
 let centimetros = distancia * 100
 
 alert(`Distancia em centimetros: ${centimetros}cm`)
-
 */
 
 // Exercicio 3
 
 /*
-
 let peso = Number(prompt("Peso: "))
 let altura = Number(prompt("Altura: "))
 
 let imc = peso / (altura * altura);
 
 alert(`Seu IMC é ${imc.toFixed(2)}`)
-
 */
+
+// Exercicio 4
+
+/*
+let n1 = prompt("Numero 1: ");
+let n2 = prompt("Numero 2: ");
+
+let soma = Number(n1) + Number(n1);
+
+alert(soma);
+*/
+
